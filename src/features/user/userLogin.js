28320a36faa4c68af46285919c6f9f3f6512e2ed
Dagm1425 <dagmawinebeyu3@gmail.com>
2023-06-13 11:17:13 +0300

    Add user data writing logic

--- src/features/user/userLogin.js
+++ src/features/user/userLogin.js
@@ -1,15 +1,24 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import { provider, auth } from "../../firebase";
+import { provider, auth, db } from "../../firebase";
 import { signInWithPopup } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import Container from "@mui/material/Container";
-// import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
 function userLogin() {
   async function signIn() {
     await signInWithPopup(auth, provider);
+    addUser(auth.currentUser);
   }
+
+  const addUser = async (user) => {
+    await setDoc(doc(db, "users", `${user.uid}`), {
+      uid: user.uid,
+      displayName: user.displayName,
+      photoUrl: user.photoURL,
+    });
+  };
 
   return (
     <Container>
