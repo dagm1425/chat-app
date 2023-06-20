0283b216fc40f66a9bb693e4b35e82941a2f1c2f
Dagm1425 <dagmawinebeyu3@gmail.com>
2023-06-20 15:50:24 +0300

    Correct initial state

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
   name: "user",
-  initialState: [],
+  initialState: {},
   reducers: {
     setUser: (state, action) => {
       return action.payload;
