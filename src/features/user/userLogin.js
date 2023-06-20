681288dd4e18547e12f8d0bd9b97191ef3f3a056
Dagm1425 <dagmawinebeyu3@gmail.com>
2023-06-20 15:49:56 +0300

    Write user data on sign-in

---
+++ src/features/user/userLogin.js
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { provider, auth, db } from "../../firebase";
+import { signInWithPopup } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
+function userLogin() {
+  async function signIn() {
+    await signInWithPopup(auth, provider);
+    addUser(auth.currentUser);
+  }
+
+  const addUser = async (user) => {
+    await setDoc(doc(db, "users", `${user.uid}`), {
+      uid: user.uid,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+    });
+  };
+
+  return (
+    <Container>
+      <Button onClick={signIn}>Sign-In with Google</Button>
+    </Container>
+  );
+}
+
+export default userLogin;
