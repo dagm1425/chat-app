71d20ff36bf6f9a7ec2551af9814aab904d7ad69
Dagm1425 <dagmawinebeyu3@gmail.com>
2023-06-21 16:49:35 +0300

    Correct private chat display name

--- src/features/chats/ChatsList.js
+++ src/features/chats/ChatsList.js
@@ -8,11 +8,15 @@ import Avatar from "@mui/material/Avatar";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
 import { Link } from "react-router-dom";
+import { selectUser } from "../user/userSlice";
 
 function ChatsList() {
   const chats = useSelector(selectChats);
+  const user = useSelector(selectUser);
 
   const list = chats.map((chat) => {
+    const otherMember = chat.members.find((member) => member.uid !== user.uid);
+
     return (
       <ListItem key={chat.chatId}>
         <Link
@@ -27,7 +31,7 @@ function ChatsList() {
             <Avatar src={chat.photoURL} />
           </ListItemAvatar>
           <Box>
-            <ListItemText primary={chat.displayName} />
+            <ListItemText primary={otherMember.displayName} />
             <ListItemText primary={chat.recentMsg} />
           </Box>
         </Link>
