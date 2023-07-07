f7ba998bb6c51e1a0332ea42894dac936185369a
Dagm1425 <dagmawinebeyu3@gmail.com>
2023-07-07 17:54:57 +0300

    Add emoji picker

--- src/features/chats/ChatMsgInput.js
+++ src/features/chats/ChatMsgInput.js
@@ -14,11 +14,14 @@ import {
 } from "firebase/firestore";
 import { v4 as uuid } from "uuid";
 import SendIcon from "@mui/icons-material/Send";
-import { IconButton } from "@mui/material";
+import { IconButton, Popover } from "@mui/material";
+import EmojiPicker from "emoji-picker-react";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 
 function ChatMsgInput({ chatId }) {
   const user = useSelector(selectUser);
   const [msg, setMsg] = useState("");
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const handleSendMsg = async () => {
     const msgId = uuid();
@@ -34,6 +37,19 @@ function ChatMsgInput({ chatId }) {
     setMsg("");
   };
 
+  const openEmojiPicker = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const closeEmojiPicker = () => {
+    setAnchorEl(null);
+  };
+
+  const addEmoji = (emojiData) => {
+    setMsg(msg + emojiData.emoji);
+    closeEmojiPicker();
+  };
+
   return (
     <Box
       sx={{
@@ -41,6 +57,7 @@ function ChatMsgInput({ chatId }) {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        gap: "0.5rem",
         position: "fixed",
         bottom: "0",
         fontSize: "1.125rem",
@@ -48,6 +65,27 @@ function ChatMsgInput({ chatId }) {
         py: "1.5rem",
       }}
     >
+      <IconButton
+        sx={{
+          size: "large",
+          "&.MuiButtonBase-root:hover": {
+            bgcolor: "transparent",
+          },
+        }}
+        onClick={openEmojiPicker}
+      >
+        <EmojiEmotionsIcon sx={{ fontSize: "inherit" }} />
+      </IconButton>
+
+      <Popover
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={closeEmojiPicker}
+      >
+        <EmojiPicker onEmojiClick={addEmoji} />
+      </Popover>
+
       <input
         type="text"
         placeholder="Enter msg"
@@ -55,7 +93,7 @@ function ChatMsgInput({ chatId }) {
         onChange={(e) => setMsg(e.target.value)}
         style={{
           font: "inherit",
-          padding: "1rem 1.75rem",
+          padding: "1rem 1.25rem",
           width: "65%",
           border: "1px solid #000",
           borderRadius: "50px",
