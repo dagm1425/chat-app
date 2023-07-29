b1afffaab921cd69a509fd70eb28c2ed5b19e1cf
Dagm1425 <dagmawinebeyu3@gmail.com>
2023-07-29 12:01:03 +0300

    Create initial mui theme

--- src/index.js
+++ src/index.js
@@ -3,14 +3,32 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
+import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+let theme = createTheme({
+  palette: {
+    // primary: {
+    //   main: "#f8ebd3",
+    // },
+    secondary: {
+      main: "#fcf7ed",
+    },
+    header: {
+      main: "#d1f5ea",
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
