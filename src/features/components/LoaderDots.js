9357e1a28567b7fc6129fbf55344c59a577ff193
Dagm1425 <dagmawinebeyu3@gmail.com>
2023-08-09 16:14:22 +0300

    Improve styles

--- src/features/components/LoaderDots.js
+++ src/features/components/LoaderDots.js
@@ -3,16 +3,23 @@ import styled, { keyframes } from "styled-components";
 
 function LoaderDots() {
   return (
-    <Wrapper>
-      <DotsDiv></DotsDiv>
-      <DotsDiv></DotsDiv>
-      <DotsDiv></DotsDiv>
-    </Wrapper>
+    <InlineDiv>
+      <Wrapper>
+        <DotsDiv></DotsDiv>
+        <DotsDiv></DotsDiv>
+        <DotsDiv></DotsDiv>
+      </Wrapper>
+    </InlineDiv>
   );
 }
 
+const InlineDiv = styled.div`
+  display: inline-block;
+`;
+
 const Wrapper = styled.div`
   display: flex;
+  width: fit-content;
   justify-content: center;
   &div:nth-child(2) {
     animation-delay: 0.2s;
@@ -29,9 +36,9 @@ to {
 `;
 
 const DotsDiv = styled.div`
-  width: 8px;
-  height: 8px;
-  margin: 1.5px 3px;
+  width: 6px;
+  height: 6px;
+  margin: 0.75px 1.5px;
   border-radius: 50%;
   background-color: #a3a1a1;
   opacity: 1;
