44628a2c0714b853c444345001867fd86c06162a
Dagm1425 <dagmawinebeyu3@gmail.com>
2023-08-11 16:17:19 +0300

    Remove functions updating discarded fields

--- src/features/chats/DeleteMsgDialogContent.js
+++ src/features/chats/DeleteMsgDialogContent.js
@@ -1,27 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { db } from "../../firebase";
-import { doc, deleteDoc, updateDoc } from "firebase/firestore";
+import { doc, deleteDoc } from "firebase/firestore";
 import { Box, Button } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { useSelector } from "react-redux";
-import { selectChats } from "./chatsSlice";
 
-function DeleteMsgDialogContent({ chatId, msgId, chatMsg, onClose }) {
-  const chats = useSelector(selectChats);
-
+function DeleteMsgDialogContent({ chatId, msgId, onClose }) {
   const deleteMsg = async () => {
-    const chat = chats.find((chat) => chat.chatId === chatId);
-
     onClose();
-
-    if (chat.recentMsg.msgId === msgId) {
-      const lastMsg = chatMsg[chatMsg.length - 2];
-
-      await updateDoc(doc(db, "chats", `${chatId}`), {
-        recentMsg: lastMsg,
-      });
-    }
 
     await deleteDoc(doc(db, "chats", `${chatId}`, "chatMessages", `${msgId}`));
   };
