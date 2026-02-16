20d40eaa58b346a07cf9e47c605a0eff5328b56c
dagem1425 <dagmawinebeyu3@gmail.com>
2026-02-16 16:55:28 +0300

    Adjust message bubble shadow depth

--- src/features/chats/ChatMsg.js
+++ src/features/chats/ChatMsg.js
@@ -69,8 +69,10 @@ function ChatMsg({
             ? "1.125rem 1.125rem 0 1.125rem"
             : "1.125rem 1.125rem 1.125rem 0",
           boxSizing: "border-box",
-          boxShadow:
-            "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;",
+          boxShadow: (theme) =>
+            theme.palette.mode === "light"
+              ? "0 1px 0.5px rgba(11, 20, 26, 0.13), 0 2px 3px rgba(11, 20, 26, 0.08)"
+              : "0 1px 0.5px rgba(0, 0, 0, 0.36), 0 2px 4px rgba(0, 0, 0, 0.24)",
         }}
       >
         {isMsgFromOtherPublicChatMembers && (
